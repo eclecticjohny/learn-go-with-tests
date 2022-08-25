326bc9c72fbf4349506da03b8b4ef5dc52f91b07
Johny Hoffman <johny.hoffman@gmail.com>
2022-08-25 08:44:10 -0500

    feat: array and slices sum

---
+++ arrays-and-slices/sum_test.go
@@ -0,0 +1,27 @@
+package arraysandslices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	// t.Run("collection of 5 numbers", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3, 4, 5}
+
+	// 	got := Sum(numbers)
+	// 	want := 15
+
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// })
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{2, 4, 6, 8}
+
+		got := Sum(numbers)
+		want := 20
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
