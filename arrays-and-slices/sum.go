c6e5e13bba86461576f4984c3185dce6474d58ed
Johny Hoffman <johny.hoffman@gmail.com>
2022-08-25 22:23:42 -0500

    refactor: array and slices sum

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,8 +1,32 @@
 package arraysandslices
 
+// Sum returns the sum of all numbers in a slice.
 func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
+	}
+
+	return
+}
+
+// SumAll returns the sum of all numbers in each slice.
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+// SumAllTails returns the sum of all numbers in each slice except the first.
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,19 +1,12 @@
 package arraysandslices
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	// t.Run("collection of 5 numbers", func(t *testing.T) {
-	// 	numbers := []int{1, 2, 3, 4, 5}
-
-	// 	got := Sum(numbers)
-	// 	want := 15
-
-	// 	if got != want {
-	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	// 	}
-	// })
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{2, 4, 6, 8}
 
@@ -24,4 +17,72 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum of some slices", func(t *testing.T) {
+		got := SumAll([]int{2, 4}, []int{6, 8})
+		want := []int{6, 14}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{})
+		want := []int{0, 0}
+
+		checkSums(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{2, 4, 6}, []int{8, 10, 12})
+		want := []int{10, 22}
+
+		checkSums(t, got, want)
+	})
+}
+
+func checkSums(t *testing.T, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]int{2, 4, 6, 8})
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{2, 4}, []int{6, 8})
+	}
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([]int{2, 4, 6}, []int{8, 10, 12})
+	}
+}
+
+func ExampleSum() {
+	repeated := Sum([]int{2, 4, 6, 8})
+	fmt.Println(repeated)
+	// Output: 20
+}
+
+func ExampleSumAll() {
+	repeated := SumAll([]int{2, 4}, []int{6, 8})
+	fmt.Println(repeated)
+	// Output: [6 14]
+}
+
+func ExampleSumAllTails() {
+	repeated := SumAllTails([]int{2, 4, 6}, []int{8, 10, 12})
+	fmt.Println(repeated)
+	// Output: [10 22]
 }
