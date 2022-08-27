bc0dc2ffbd8991c3cd9e0df014c9e1f765bee2a0
Johny Hoffman <johny.hoffman@gmail.com>
2022-08-27 15:35:11 -0500

    refactor: add concurrency

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,21 +1,41 @@
 package main
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
 }
 
-// slow version of website checker benchmark
+// non-concurrent version of website checker benchmark
 // goos: darwin
 // goarch: arm64
 // pkg: github.com/eclecticjohny/learn-go-with-tests/concurrency
 // BenchmarkCheckWebsites-8   	       1	2086890375 ns/op	     112 B/op	       4 allocs/op
 // PASS
 // ok  	github.com/eclecticjohny/learn-go-with-tests/concurrency	2.415s
+
+// concurrent version of website checker benchmark
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/eclecticjohny/learn-go-with-tests/concurrency
+// BenchmarkCheckWebsites-8   	      56	  21230214 ns/op	   17697 B/op	     319 allocs/op
+// PASS
+// ok  	github.com/eclecticjohny/learn-go-with-tests/concurrency	1.307s
