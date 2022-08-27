f3fb77eb48cfff22f089bd390b256168563769df
Johny Hoffman <johny.hoffman@gmail.com>
2022-08-27 12:01:30 -0500

    fix: mocking

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 func TestCountdown(t *testing.T) {
 	t.Run("countdown of 3", func(t *testing.T) {
@@ -23,12 +48,15 @@ func TestCountdown(t *testing.T) {
 			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 		}
 	})
-}
 
-type SpySleeper struct {
-	Calls int
-}
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+		want := []string{write, sleep, write, sleep, write, sleep, write}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
