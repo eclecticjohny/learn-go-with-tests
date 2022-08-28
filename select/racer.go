342cc5c57ea90ab41f42f3292d1f82bcbd1c167b
Johny Hoffman <johny.hoffman@gmail.com>
2022-08-27 20:55:16 -0500

    fix: add comments

--- select/racer.go
+++ select/racer.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
+	// ping a and b concurrently
 	select {
 	case <-ping(a):
 		return a
@@ -14,10 +15,15 @@ func Racer(a, b string) (winner string) {
 }
 
 func ping(url string) chan struct{} {
+	// make a channel to receive the result
 	ch := make(chan struct{})
+	// start a goroutine to make the HTTP request
 	go func() {
+		// make the HTTP request and close the channel
 		http.Get(url)
+		// signal that the request has been completed
 		close(ch)
 	}()
+	// return the channel
 	return ch
 }
